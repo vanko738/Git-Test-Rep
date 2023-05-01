52d1740733c2ed8a0ad61e0b6b3578935c9a5259
vanko738 <ivanlarsson90@gmail.com>
2023-05-01 11:34:54 +0200

    sdasd

--- as.js
+++ as.js
@@ -2,10 +2,8 @@ asdasdasdasdasdasd
 
 
 
-"sdasdasasnsdndsmn kjsngdnskasnasndkasdnjasdkjnsd" 
  
 
-console.log("asdasasdasdddddddddddddddddddddddddddddddddddddasasdasdasdas");
 
 console.log("Test asd")
 
